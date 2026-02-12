4bb41100dfe3b530cc08b0f3700e0af486b57202
ImFeH2 <i@feh2.im>
2026-02-12 16:49:26 +0800

    feat: initialize Fastapi + React project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import '@/styles/App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Welcome to the App!</h1>
+    </>
+  )
+}
+
+export default App;
